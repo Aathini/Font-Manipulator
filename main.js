2259b517c50c0cb5d20fd39f2211301207d25564
Aathini <78332561+Aathini@users.noreply.github.com>
2022-04-29 21:53:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     canvas = createCanvas(300,300)
     canvas.position(550,200);
@@ -10,6 +12,22 @@ function setup() {
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose',gotPoses);
 }
-function gotPoses() {}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = resuls[0].pose.leftWrist.X;
+        rightWristX = resuls[0].pose.rightWrist.X;
+        difference = floor(leftWristX - rightWristX);
+    }
+
+}
 function modelLoaded() {
-    console.log('Model has been loaded'); }
+    console.log('PoseNet has been Initialized'); }
+function draw() {
+    background("#defffe");
+
+    textSize(difference);
+    fill("#FFE787");
+    text('Aathini', 50, 400);
+}
